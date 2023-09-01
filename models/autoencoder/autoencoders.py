cbe99870f741c5e82805f2a9542e6baffb2f88b5
hoang1645 <hoang.208.1974@gmail.com>
2023-09-01 16:58:31 +0700

    Revert "conduct a test for cnn autoencoder"
    This reverts commit fd30eda5d5750b08f4bd186d368cfb7976d6de14.

--- models/autoencoder/autoencoders.py
+++ models/autoencoder/autoencoders.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn 
 import torch.nn.functional as F
-from .cnn.encoder import LatentSpaceEncoder
-from .cnn.decoder import LatentSpaceDecoder
+from ._transformers.encoder import LatentSpaceEncoder
+from ._transformers.decoder import LatentSpaceDecoder
 from typing import Any, Callable
 from vector_quantize_pytorch import VectorQuantize
 from abc import abstractmethod
@@ -11,12 +11,10 @@ from einops import rearrange
 
     
 class Autoencoder(nn.Module):
-    def __init__(self, conv_init_chan=32, chan_mults=(1,2,3,4)
-                 #kernel_size, n_heads:int, d_model:int, n_layers:int, dropout:float, activation:Callable[..., torch.Tensor]
-                 ):
+    def __init__(self, kernel_size, n_heads:int, d_model:int, n_layers:int, dropout:float, activation:Callable[..., torch.Tensor]):
         super().__init__()
-        self.encoder = LatentSpaceEncoder(conv_init_chan=conv_init_chan, chan_mults=chan_mults)
-        self.decoder = LatentSpaceDecoder(conv_init_chan=conv_init_chan, chan_mults=list(chan_mults)[::-1])
+        self.encoder = LatentSpaceEncoder(kernel_size, n_heads, d_model, n_layers, dropout, activation)
+        self.decoder = LatentSpaceDecoder(kernel_size, n_heads, d_model, n_layers, dropout, activation)
         
     def forward(self, x:torch.Tensor):
         return self.encoder(self.decoder(x))
@@ -25,25 +23,25 @@ class Autoencoder(nn.Module):
     def reg_loss(self, _input:torch.Tensor):
         raise NotImplementedError()
 
-# class AutoencoderKL(Autoencoder):
-#     def __init__(self, kernel_size:int, d_model:int, n_heads:int, n_layers:int, dropout:float, activation:Callable[..., torch.Tensor],
-#                  kl_penalty:float=1e-6):
-#         super().__init__(kernel_size, n_heads, d_model, n_layers, dropout, activation)
-#         self.regularization_loss = nn.KLDivLoss()
-#         self.kl_penalty = kl_penalty
+class AutoencoderKL(Autoencoder):
+    def __init__(self, kernel_size:int, d_model:int, n_heads:int, n_layers:int, dropout:float, activation:Callable[..., torch.Tensor],
+                 kl_penalty:float=1e-6):
+        super().__init__(kernel_size, n_heads, d_model, n_layers, dropout, activation)
+        self.regularization_loss = nn.KLDivLoss()
+        self.kl_penalty = kl_penalty
 
-#     def reg_loss(self, _input:torch.Tensor):
-#         target = torch.randn_like(_input)
-#         return self.kl_penalty * self.regularization_loss.forward(_input, target)
+    def reg_loss(self, _input:torch.Tensor):
+        target = torch.randn_like(_input)
+        return self.kl_penalty * self.regularization_loss.forward(_input, target)
     
 class AutoencoderVQ(Autoencoder):
-    def __init__(self, quant_dim:int, codebook_size:int=512, conv_init_chan=32, chan_mults=(1,2,3,4),
-                 ):
-        super().__init__(conv_init_chan=conv_init_chan, chan_mults=chan_mults)
+    def __init__(self, kernel_size, d_model:int, n_heads:int, n_layers:int, dropout:float, activation:Callable[..., torch.Tensor],
+                 quant_dim:int, codebook_size:int=512):
+        super().__init__(kernel_size, n_heads, d_model, n_layers, dropout, activation)
         self.codebook_size = codebook_size
         self.vquantizer = VectorQuantize(quant_dim, self.codebook_size, learnable_codebook=True, channel_last=False, ema_update=False)
-        self.quant_conv = nn.Conv2d(32, quant_dim, 1)
-        self.post_quant_conv = nn.Conv2d(quant_dim, 32, 1)
+        self.quant_conv = nn.Conv2d(d_model // kernel_size, quant_dim, 1)
+        self.post_quant_conv = nn.Conv2d(quant_dim, d_model // kernel_size, 1)
 
     def encode(self, x:torch.Tensor):
         x = self.encoder(x)
