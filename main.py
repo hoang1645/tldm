666e252b96230687df9cacdffc105a757b35a7b8
hoang1645 <hoang.208.1974@gmail.com>
2023-09-11 18:42:00 +0700

    change unet dims

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
     args = parse_args()
     print(args)
 
-    unet = UNet(in_chan=32, out_chan=32, embed_dim=128, n_attn_heads=8, dim_head=64, conv_init_chan=128, chan_mults=(1,2,4,8))
+    unet = UNet(in_chan=32, out_chan=32, embed_dim=160, n_attn_heads=8, dim_head=64, conv_init_chan=128, chan_mults=(1,2,3,4))
     autoencoder = AutoencoderVQ(64, quant_dim=32, codebook_size=16384)
     model = LDM(unet, autoencoder, 256)
     d_optim = torch.optim.AdamW(model.unet.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
